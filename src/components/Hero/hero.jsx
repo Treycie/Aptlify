38f430730608ec9e41a0de7ddc9ac0e6419d800a
unknown <edvalerie18@gmail.com>
2024-03-08 20:46:32 +0000

    worked on landing page

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div
+      className="hero min-h-screen flex items-center"
+      style={{
+        backgroundImage: `url(${require("../../assets/Images/hero-bg.jpg")})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content flex flex-col justify-center items-center text-neutral-content h-full">
+        <div className="max-w-md">
+          <h1
+            className="mb-100 text-5xl font-bold ml-4"
+            style={{ fontSize: "1.5rem", color: "red" }}
+          >
+            APTIFY
+          </h1>
+          <p
+            className="mb-10 font-bold ml-4"
+            style={{ fontSize: "3.0rem", color: "white" }}
+          >
+            Unlimited Movies, TV Shows,& More.
+          </p>
+          <button
+            type="button"
+            class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800 ml-4"
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
