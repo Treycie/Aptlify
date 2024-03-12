fc17111bc5d9000cf87cd8beced2f406faab5294
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-12 08:52:28 -0700

    created new page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Landing from "./pages/Landingpage/landing"
 import Signup from "./pages/Signup/signup"
 import Signin from "./pages/Signin/signin"
 import Getstarted from "./pages/Getstarted/getstarted"
+import Topratedmovies from "./pages/Topratedmovies/Topratedmovies"
 
 const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/signin", element: <Signin /> },
   { path: "/signup", element: <Signup /> },
   { path: "/getstarted", element: <Getstarted /> },
+  { path: "/topratedmovies", element: <Topratedmovies/>}
 ])
 const App = () => {
   return (
