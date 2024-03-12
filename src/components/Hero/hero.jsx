76f65a2dd5a98f4c8cb57da0a7a6b1e736d3b59a
unknown <edvalerie18@gmail.com>
2024-03-12 12:39:32 +0000

    repositioned hero image

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom/dist";
 const Hero = () => {
   return (
     <div
       className="hero min-h-screen flex items-center"
       style={{
-        backgroundImage: `url(${require("../../assets/Images/hero-bg.jpg")})`,
+        background: `url(${require("../../assets/Images/some-tale-2.jpg")})`,
         backgroundSize: "cover",
-        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "right",
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
@@ -20,22 +22,24 @@ const Hero = () => {
           </h1>
           <p
             className="font-bold ml-4 whitespace-nowrap"
-            style={{ fontSize: "3.8rem", color: "white" }}
+            style={{ fontSize: "3.2rem", color: "white" }}
           >
             Unlimited Movies,
           </p>
           <p
             className="mb-10 font-bold ml-4 whitespace-nowrap"
-            style={{ fontSize: "3.8rem", color: "white" }}
+            style={{ fontSize: "3.2rem", color: "white" }}
           >
             TV Shows,& More.
           </p>
+          <Link to="/getstarted">
           <button
             type="button"
             class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800 ml-4"
           >
             Get Started
           </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Moviedetail/moviedetail.jsx
+++ src/pages/Moviedetail/moviedetail.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/footer";
 
 const Moviedetail = () => {
+  const {results} = useQueryPopular();
+
   return (
     <>
       <Navbar />
