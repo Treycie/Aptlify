26cb7ee074d4378ca03a8a4fe9ce89307932bbe4
Konkuri1 <konkurievarest363@gmail.com>
2024-03-15 09:03:29 +0000

    Items added to the popularmovies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Landing from "./pages/Landingpage/landing"
-import Signup from "./pages/Signup/signup"
-import Signin from "./pages/Signin/signin"
-import Getstarted from "./pages/Getstarted/getstarted"
-
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Landing from "./pages/Landingpage/landing";
+import Signup from "./pages/Signup/signup";
+import Signin from "./pages/Signin/signin";
+import Getstarted from "./pages/Getstarted/getstarted";
+import Popularmovies from "./pages/Popularmovies/popularmovies";
 
 const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/signin", element: <Signin /> },
   { path: "/signup", element: <Signup /> },
   { path: "/getstarted", element: <Getstarted /> },
+  { path: "/popularmovies", element: <Popularmovies />},
+ 
 ])
 const App = () => {
   return (
