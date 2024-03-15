b04c153a2b0981e1783721544bfed5c4b1c8c8dd
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-15 03:43:46 -0700

    upcoming

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signup from "./pages/Signup/signup"
 import Signin from "./pages/Signin/signin"
 import Getstarted from "./pages/Getstarted/getstarted"
 import Topratedmovies from "./pages/Topratedmovies/Topratedmovies"
+import Upcomingmovies from "./pages/Upcomingmovies/Upcomingmovies"
 
 
 
@@ -12,7 +13,8 @@ const router = createBrowserRouter([
   { path: "/signin", element: <Signin /> },
   { path: "/signup", element: <Signup /> },
   { path: "/getstarted", element: <Getstarted /> },
-  { path: "/topratedmovies", element: <Topratedmovies /> }
+  { path: "/topratedmovies", element: <Topratedmovies /> },
+  {path: "/upcomingmovies", element: <Upcomingmovies/>}
 ])
 const App = () => {
   return (
