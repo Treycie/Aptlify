dba7d930fa815e013b63f55b664772ac8346b19d
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-15 07:28:10 -0700

    change

--- src/pages/Moviedetail/moviedetail.jsx
+++ src/pages/Moviedetail/moviedetail.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 // import Footer from "../../components/Footer/footer";
 // import Navbar from "../../components/Navbar/navbar";
-import { useLocation, useParams } from "react-router";
+import { useLocation } from "react-router";
 
 const Moviedetail = () => {
   const location = useLocation();
@@ -13,29 +13,30 @@ const Moviedetail = () => {
       <div className="min-h-screen w-full mt-0 bg-black">
         <div className="mt-0 flex items-center justify-center text-white text-2xl font-bold">
           <div className="mt-10 ml-10">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={`movie`}
-            className="max-w-xs transition-opacity hover:opacity-20"
-          />
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={`movie`}
+              className="max-w-xs transition-opacity hover:opacity-20"
+            />
           </div>
           <div className="ml-28 mt-16">
-              <h1>Title: {movie.title}</h1>
-              <div className="pt-2" />
-              {/* <p>Genres: {genres.join(', ')}</p> */}
-              <div className="pt-2" />
-              <p>Year: {movie.release_date}</p>
-              <div className="pt-2" />
-              <p>Language: {movie.original_language}</p>
-              <div className="pt-2" />
-              <p>Rating: {movie.vote_average}</p>
-              <div className="pt-2" />
-              <p>Popularity: {movie.popularity}</p>
-              <div className="pt-2" />
-              <h2>Overview
-                <p>{movie.overview}</p></h2>
-              <div className="pt-12" />
-
+            <h1>Title: {movie.title}</h1>
+            <div className="pt-2" />
+            {/* <p>Genres: {genres.join(', ')}</p> */}
+            <div className="pt-2" />
+            <p>Year: {movie.release_date}</p>
+            <div className="pt-2" />
+            <p>Language: {movie.original_language}</p>
+            <div className="pt-2" />
+            <p>Rating: {movie.vote_average}</p>
+            <div className="pt-2" />
+            <p>Popularity: {movie.popularity}</p>
+            <div className="pt-2" />
+            <h2>
+              Overview
+              <p>{movie.overview}</p>
+            </h2>
+            <div className="pt-12" />
           </div>
         </div>
       </div>
